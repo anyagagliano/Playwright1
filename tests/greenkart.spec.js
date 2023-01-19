70c56a6f386f34d3a6412d00caf0d58bbb2d22a4
anyagagliano <anyagagliano87@gmail.com>
2023-01-19 15:48:45 +0100

    main-page.js is added

--- tests/greenkart.spec.js
+++ tests/greenkart.spec.js
@@ -1,26 +1,20 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
-
-test.beforeEach(async ({ page }) => {
-    await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/');
-  });
-
-test('Page has right title', async ({ page }) => {
-    await expect(page).toHaveTitle(/GreenKart - veg and fruits kart/);
-  });
+const { MainPage } = require('../pages/main-page');
 
 
 test.describe('New purchase', () => {
 
-  test('Search for tomato', async ({ page }) => {
-    const searchField = page.getByPlaceholder('Search for Vegetables and Fruits');
-    const tomato = page.getByRole('heading', { name: 'Tomato - 1 Kg' });
-    await searchField.fill('tomato');
-    await page.getByRole('banner').getByRole('button').click();
-    await expect(tomato).toBeVisible();
+  test('Search for different vegetables', async ({ page }) => {
+    const greenkart = new MainPage(page);
+    await greenkart.goto();
+    await greenkart.searchForProduct();
+    await expect(greenkart.product).toBeVisible();
 })
   test('Cart contains right amount of items', async ({ page }) => {
     const list = page.locator('ul.cart-items');
+    const greenkart = new MainPage(page);
+    await greenkart.goto();
     await page.locator('.product-action > button').first().click();
     await page.locator('div:nth-child(2) > .product-action > button').click();
     await page.locator('.cart-icon').click();
@@ -28,6 +22,8 @@ test.describe('New purchase', () => {
     await expect(list).toContainText(['Brocolli - 1 Kg','Cauliflower - 1 Kg']);
 })
   test('Complete purchase', async ({ page }) => {
+    const greenkart = new MainPage(page);
+    await greenkart.goto();
     const productTable = page.locator('#productCartTables');
     const list = page.locator('ul.cart-items');
     const totalAmount = page.locator('.discountAmt');
