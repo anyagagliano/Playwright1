8c90fca8ad254f7e711ea623d30867a6c9d2e18a
anyagagliano <anyagagliano87@gmail.com>
2023-01-24 11:28:00 +0100

    added cart and country pages

--- pages/main-page.js
+++ pages/main-page.js
@@ -12,6 +12,11 @@ exports.MainPage = class MainPage {
     this.searchfield = page.getByPlaceholder('Search for Vegetables and Fruits');
     this.searchBtn = page.getByRole('banner').getByRole('button');
     this.product = page.getByRole('heading', { name: 'Tomato - 1 Kg' });
+    this.brocolli = page.locator('.product-action > button').first();
+    this.cauliflower = page.locator('div:nth-child(2) > .product-action > button');
+    this.cart = page.locator('.cart-icon');
+    this.checkoutBtn = page.getByRole('button', { name: 'PROCEED TO CHECKOUT' });
+    this.list = page.locator('ul.cart-items');
   }
 
   async goto() {
@@ -25,4 +30,17 @@ exports.MainPage = class MainPage {
     await expect(this.product).toBeVisible();
   }
 
+  async addProducts() {
+    await this.brocolli.click();
+    await this.cauliflower.click();
+  }
+
+  async openCart() {
+    await this.cart.click();
+  }
+
+  async proceedToCheckout() {
+    await this.cart.click();
+    await this.checkoutBtn.click()
+  }
 }
--- tests/greenkart.spec.js
+++ tests/greenkart.spec.js
@@ -1,5 +1,7 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
+const { CartPage } = require('../pages/cart-page');
+const { CountryPage } = require('../pages/country-page');
 const { MainPage } = require('../pages/main-page');
 
 
@@ -12,40 +14,37 @@ test.describe('New purchase', () => {
     await expect(greenkart.product).toBeVisible();
 })
   test('Cart contains right amount of items', async ({ page }) => {
-    const list = page.locator('ul.cart-items');
     const greenkart = new MainPage(page);
     await greenkart.goto();
-    await page.locator('.product-action > button').first().click();
-    await page.locator('div:nth-child(2) > .product-action > button').click();
-    await page.locator('.cart-icon').click();
-    await expect(list).toHaveCount(2); 
-    await expect(list).toContainText(['Brocolli - 1 Kg','Cauliflower - 1 Kg']);
+    await greenkart.addProducts();
+    await greenkart.openCart();
+    await expect(greenkart.list).toHaveCount(2); 
+    await expect(greenkart.list).toContainText(['Brocolli - 1 Kg','Cauliflower - 1 Kg']);
 })
   test('Complete purchase', async ({ page }) => {
     const greenkart = new MainPage(page);
+    const cart = new CartPage(page);
+    const country = new CountryPage(page);
     await greenkart.goto();
-    const productTable = page.locator('#productCartTables');
-    const list = page.locator('ul.cart-items');
-    const totalAmount = page.locator('.discountAmt');
-    await page.locator('.product-action > button').first().click();
-    await page.locator('div:nth-child(2) > .product-action > button').click();
-    await page.locator('.cart-icon').click();
-    await page.getByRole('button', { name: 'PROCEED TO CHECKOUT' }).click();
-    await expect(productTable).toBeVisible();
-    await expect(totalAmount).toHaveText('180');
-    await page.getByRole('button', { name: 'Place Order' }).click();
-    await page.getByRole('combobox').selectOption('Sweden');
-    await expect(page.getByRole('combobox')).toContainText('Sweden');
-    await page.getByRole('checkbox').check();
-    await expect(page.getByRole('checkbox')).toBeChecked(); 
-    const [page1] = await Promise.all([
-        page.waitForEvent('popup'),
-        page.getByRole('link', { name: 'Terms & Conditions' }).click()
-    ]);
-    await page1.getByRole('link', { name: 'Home' }).click();
-    await page.getByRole('button', { name: 'Proceed' }).click();
-    await expect(list).toHaveCount(0);
+    await greenkart.addProducts();
+    await greenkart.proceedToCheckout();
+    await expect(cart.productTable).toBeVisible();
+    await expect(cart.totalAmount).toHaveText('180');
+    await cart.placeOrder();
+    await country.selectCountry();
+    await expect(country.countryDropDown).toContainText('Sweden');
+    await country.checkBoxTC();
+    await expect(country.checkbox).toBeChecked(); 
+   /* const [page1] = await Promise.all([
+      page.waitForEvent('popup'),
+      page.getByRole('link', { name: 'Terms & Conditions' }).click()
+  ]);
+  await page1.getByRole('link', { name: 'Home' }).click();
+  */
+  await country.proceed();
+    await expect(greenkart.list).toHaveCount(0);
 });
+  
 
   })
 
